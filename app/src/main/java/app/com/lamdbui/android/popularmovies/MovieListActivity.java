60a5eb9cdf69ca702d27a40091611dd6a2f009dc
Lam Bui <lamdbui@gmail.com>
2017-01-08 21:58:49 -0800

    Start separating the FetchMovieDBTask into its own class. Still need to cleanup

--- app/src/main/java/app/com/lamdbui/android/popularmovies/MovieListActivity.java
+++ app/src/main/java/app/com/lamdbui/android/popularmovies/MovieListActivity.java
@@ -5,7 +5,22 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.stetho.Stetho;
+
 public class MovieListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Stetho.initializeWithDefaults(this);
+
+        Stetho.initialize(Stetho.newInitializerBuilder(this)
+                .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                .build()
+        );
+    }
 
     @Override
     protected Fragment createFragment() {
